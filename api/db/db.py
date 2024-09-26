144b6ea11a7f90abab4c507fd895e778bc2e4f3d
JorgeRico <jorge.rico.garriga@gmail.com>
2024-09-26 15:38:11 +0200

    api db exceptions

--- api/db/db.py
+++ api/db/db.py
@@ -4,32 +4,41 @@ from mysql.connector import Error
 class Db:
 
     def __init__(self):
-        self.hostName        = 'app-database'
-        self.portName        = '3308'
-        self.databaseName    = 'app-database'
-        self.userName        = 'user'
-        self.passwordText    = 'password'
+        self.hostName     = 'app-database'
+        self.portName     = '3308'
+        self.databaseName = 'app-database'
+        self.userName     = 'user'
+        self.passwordText = 'password'
 
     def connect(self):
-        return mysql.connector.connect(
-            host     = self.hostName,
-            user     = self.userName,
-            password = self.passwordText,
-            database = self.databaseName
-        )
+        try:
+            return mysql.connector.connect(
+                host     = self.hostName,
+                user     = self.userName,
+                password = self.passwordText,
+                database = self.databaseName
+            )   
+        except Error as e:
+            print("Error while connecting to MySQL %s" %e)
     
     def getSelectListResultQuery(self, conn, query):
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute(query)
-        result = cursor.fetchall()
-        conn.close()
+        try:
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute(query)
+            result = cursor.fetchall()
+            conn.close()
 
-        return result
+            return result
+        except Error as e:
+            print("Error while connecting to MySQL %s" %e)
     
     def getSelectSingleResultQuery(self, conn, query):
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute(query)
-        result = cursor.fetchone()
-        conn.close()
+        try:
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute(query)
+            result = cursor.fetchone()
+            conn.close()
 
-        return result
+            return result
+        except Error as e:
+            print("Error while connecting to MySQL %s" %e)
